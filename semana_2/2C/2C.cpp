105f3e87a046d32f81c5c825e09985fb51d4b0f9
Allan Pedro <allanpedro.sds@gmail.com>
2020-03-06 02:18:48 -0300

    done

--- semana_2/2C/2C.cpp
+++ semana_2/2C/2C.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <cstdio>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    int k, l = 1, hh_1, hh_2, h_aux, h_diff = 0, h_star, mm_1, mm_2, m_aux, m_diff = 0, m_star, diff = 0, bigg = 0;
-    char appoint[225];
+    int k, l = 1, hh_1, hh_2, h_aux = 0, h_diff = 0, h_star = 0, mm_1, mm_2, m_aux = 0, m_diff = 0, m_star = 0, diff = 0, bigg = 0;
 
     while (true)
     {
         cin >> k;
-        cout << k;
 
         if (k > 100)
             break;
 
         for (int i = 0; i < k; i++)
         {
-            fflush(stdin);
-            scanf("%d:%d %d:%d %s", &hh_1, &mm_1, &hh_2, &mm_2, &appoint);
-            cout << hh_1 << endl << mm_1 << endl << hh_2 << endl << mm_2 << endl;
+            scanf("%d:%d %d:%d", &hh_1, &mm_1, &hh_2, &mm_2);
 
             if (i > 0)
             {
@@ -34,16 +31,17 @@ int main()
                     h_diff = hh_1 - h_aux;
                     m_diff = 0;
                 }
-                else if ((hh_1 + 1) == h_aux && mm_1 != m_aux)
+                else if (hh_1 == (h_aux + 1) && mm_1 != m_aux)
                 {
                     h_diff = 0;
-                    m_diff = 60 - h_aux + hh_1;
+                    m_diff = 60 - m_aux + mm_1;
                 }
                 else if ((hh_1 + 1) > h_aux && mm_1 != m_aux)
                 {
                     h_diff = hh_1 - h_aux;
-                    m_diff = 60 - h_aux + hh_1;
+                    m_diff = 60 - m_aux + mm_1;
                 }
+            }
             diff = h_diff*60 + m_diff;
 
             if (diff > bigg)
@@ -54,14 +52,16 @@ int main()
             }
             h_aux = hh_2;
             m_aux = mm_2;
-            }
         }
 
         if(bigg < 60)
-            cout << "Day #" << l << ": the longest nap starts at " << h_star << ":" << m_star << "and will last for " << bigg << " minutes." << endl;
+            cout << "Day #" << l << ": the longest nap starts at " << h_star << ":" << setw(2) << setfill('0') << m_star << " and will last for " << bigg << " minutes." << endl;
         else
-            cout << "Day #" << l << ": the longest nap starts at " << h_star << ":" << m_star << "and will last for " << (bigg/60) << " hours and " << (bigg%60) << " minutes." << endl;
+            cout << "Day #" << l << ": the longest nap starts at " << h_star << ":" << setw(2) << setfill('0') << m_star << " and will last for " << (bigg/60) << " hours and " << (bigg%60) << " minutes." << endl;
         l++;
+        h_diff = 0;
+        m_diff = 0;
+        bigg = 0;
     }
     return 0;
 }

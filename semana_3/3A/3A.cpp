d852aa39415cc2101ed5e4462da35c9ab60663ab
allanpss <allanpss@lcc-28.lcc.ufrn.br>
2020-03-06 16:07:05 -0300

    Reading input

--- semana_3/3A/3A.cpp
+++ semana_3/3A/3A.cpp
@@ -2,14 +2,39 @@
 
 using namespace std;
 
+
+
 int main()
 {
-    int n, m;
+    int n, m, t, k = 0;
     cin >> n >> m; // Leitura
-    int p[m];
+
+    if (n < 1 || m > 10000)
+        return 0;
+
+    int p[m], o[m] = {0};
+    bool run[m];
     for (int j = 0; j < m; j++)
         cin >> p[j];
-    for (int j = 0; j < m; j++)
-        cout << p[j];
+
+    for (int i = 0; i < m; i++)
+    {
+        t = p[i];
+        if (i == 0)
+            k++;
+        else
+        {
+            if (t == p[i-1])
+                k++;
+        }
+        cout << k << endl;
+        if (k == n)
+            o[i] = 1;
+
+    }
+    
+    for (int i = 0; i < m; i++)
+        cout << o[i];
+
     return 0;
 }
